c01af2a66b22952c0b7ac34c869e783e9186c7a8
class60 <class6051@gmail.com>
2019-04-29 04:53:28 +0700

    andi

---
+++ AppCordova/AppCordova/AppCordova/www/db.php
@@ -0,0 +1,4 @@
+<?php
+ header("Access-Control-Allow-Origin: *");
+ $con = mysqli_connect("localhost","id8365689_appcordova","andi23","id8365689_appcordova") or die ("could not connect database");
+?>
